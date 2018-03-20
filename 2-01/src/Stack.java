34461c891a8ddb0a9c692838df5365331040a783
JamesSanii <James.sanii@ocsbstudent.ca>
2018-03-20 13:38:20 -0400

    Add files via upload

---
+++ 2-01/src/Stack.java
@@ -0,0 +1,15 @@
+/****************************************************************************
+ *
+ * Created by: James Sanii
+ * Created on: March 2018
+ * This program is an example of a stack 
+ *
+ ****************************************************************************/
+import java.util.ArrayList;
+
+public class Stack {
+	private ArrayList<Integer> list = new ArrayList<Integer>();
+	public void push(int number) {
+		list.add(number);
+	}
+}
